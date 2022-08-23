025ad1351c9730649f8cdf8b720411061b98a9f6
Brennan Davis <brennantd@gmail.com>
2022-08-23 13:47:31 -0600

    added BookNav component in Book page

---
+++ src/components/BookNav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function BookNav() {
+    return (
+        <div id="book-nav">
+            <ul>
+                <li>Fiction</li>
+                <li>Nonfiction</li>
+                <li>eBooks</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookNav
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import BookNav from './BookNav'
 
 function Books() {
     return (
         <div>
-            <p>This is the Books page. This page will be full of book collections in a variety of categories.</p>
+            <BookNav />
         </div>
     )
 }
