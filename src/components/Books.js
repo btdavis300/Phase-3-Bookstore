a70423fddd38d15fa00a5391045c417a4c98f816
Brennan Davis <brennantd@gmail.com>
2022-08-22 20:02:08 -0600

    client-side routing is working

---
+++ src/components/Books.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Books() {
+    return (
+        <div>
+            <p>This is the Books page. This page will be full of book collections in a variety of categories.</p>
+        </div>
+    )
+}
+
+export default Books
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <p>This is the Home Page. This will be filled with books and eye-grabbing information.</p>
+        </div>
+    )
+}
+
+export default Home
