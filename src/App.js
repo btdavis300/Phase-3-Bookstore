e06097e56b793ab25eee4b21caef0b9ca34d58fe
Brennan Davis <brennantd@gmail.com>
2022-08-24 12:47:30 -0600

    finished BookNav functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from "react-router-dom";
 import NavBar from './components/NavBar';
 import Home from './components/Home';
@@ -9,42 +9,42 @@ import Signup from './components/Signup';
 
 function App() {
 
-  
 
-  const [books, setBooks] = useState([]);
-  const [userAuthorized,setUserAuthorized] = useState(false);
-  const [adminAuthorized,setAdminAuthorized] = useState(false);
-  
 
-  useEffect(() => {
-    fetch("http://localhost:9292/all")
-      .then(r => r.json())
-      .then(bookArr => setBooks(bookArr))
-  }, [])
+  // const [books, setBooks] = useState([]);
+  const [userAuthorized, setUserAuthorized] = useState(false);
+  const [adminAuthorized, setAdminAuthorized] = useState(false);
 
-  console.log(books)
 
-  
+  // useEffect(() => {
+  //   fetch("http://localhost:9292/all")
+  //     .then(r => r.json())
+  //     .then(bookArr => setBooks(bookArr))
+  // }, [])
+
+  // console.log(books)
+
+
   return (
     <div className="App">
-      <NavBar userAuthorized={userAuthorized} setUserAuthorized={setUserAuthorized} adminAuthorized={adminAuthorized} setAdminAuthorized={setAdminAuthorized}/>
+      <NavBar userAuthorized={userAuthorized} setUserAuthorized={setUserAuthorized} adminAuthorized={adminAuthorized} setAdminAuthorized={setAdminAuthorized} />
       <div id="nav">
         <Switch>
           <Route exact path="/">
-            <Home books={books} />
+            <Home />
           </Route>
           <Route exact path="/books">
             <Books userAuthorized={userAuthorized} />
           </Route>
-          
+
           <Route exact path="/login">
-            <Login userAuthorized={userAuthorized} setUserAuthorized={setUserAuthorized} setAdminAuthorized={setAdminAuthorized}/>
-            </Route>
-            <Route exact path="/signup">
-              <Signup />
-              </Route>
+            <Login userAuthorized={userAuthorized} setUserAuthorized={setUserAuthorized} setAdminAuthorized={setAdminAuthorized} />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
         </Switch>
-        
+
       </div>
     </div>
   );
--- src/components/BookNav.js
+++ src/components/BookNav.js
@@ -1,49 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BookCard from './BookCard'
 
 function BookNav() {
+    const [category, setCategory] = useState([])
+
     function handleCategory() {
         console.log("I am clicked!")
     }
 
-    function showSubCategory() {
-        // subCategory = document.getElementById("sub-category-nav")
-        // console.log(subCategory)
+    function handleSubcategory(e) {
+        if (e.target.textContent.includes("/")) {
+            let text = e.target.textContent.toLowerCase().replace("/", "_")
+
+            fetchCategory(text)
+        }
+        else if (e.target.textContent === "eBooks") {
+            fetch("http://localhost:9292/books/ebooks")
+                .then(r => r.json())
+                .then(data => setCategory(data))
+        } else {
+            let text = e.target.textContent.toLowerCase().replace(" ", "-")
+
+            fetchCategory(text)
+        }
     }
 
-    function hideSubCategory() {
-        console.log("I have left!")
+    function fetchCategory(text) {
+        fetch(`http://localhost:9292/books/genre/${text}`)
+            .then(r => r.json())
+            .then(data => setCategory(data))
     }
+
+    const bookResults = category.map((book) => {
+        return (
+            <BookCard book={book} key={book.id} />
+        )
+    })
 
     return (
         <div id="book-nav">
             <ul id="main-cat">
-                <li onClick={handleCategory}
-                    onMouseEnter={showSubCategory}
-                    onMouseLeave={hideSubCategory}
-                    className="category-nav">
-                    Fiction</li>
-                <li onClick={handleCategory} className="category-nav">Nonfiction</li>
-                <li onClick={handleCategory} className="category-nav">eBooks</li>
+                <li className="category-nav">Fiction</li>
+                <li className="category-nav">Nonfiction</li>
+                <li onClick={handleSubcategory} className="category-nav">eBooks</li>
             </ul>
-            <div id="sub-category-nav">
+            <div className="sub-category-nav">
                 <ul className="sub-cat">
-                    <li>Classic</li>
-                    <li>Crime</li>
-                    <li>Fiction in verse</li>
-                    <li>Folklore</li>
-                    <li>General Fiction</li>
-                    <li>Graphic Novel</li>
-                    <li>Horror</li>
+                    <li onClick={handleSubcategory}>Classic</li>
+                    <li onClick={handleSubcategory}>Crime/Detective</li>
+                    <li onClick={handleSubcategory}>Fiction in verse</li>
+                    <li onClick={handleSubcategory}>Folklore</li>
+                    <li onClick={handleSubcategory}>Fanfiction</li>
+                    <li onClick={handleSubcategory}>Realistic fiction</li>
+                    <li onClick={handleSubcategory}>Horror</li>
+                    <li onClick={handleSubcategory}>Historical fiction</li>
                 </ul>
                 <ul className="sub-cat">
-                    <li>Historic Fiction</li>
-                    <li>Humor</li>
-                    <li>Mystery</li>
-                    <li>Science Fiction</li>
-                    <li>Suspense/Thriller</li>
-                    <li>Western</li>
+                    <li onClick={handleSubcategory}>Humor</li>
+                    <li onClick={handleSubcategory}>Metafiction</li>
+                    <li onClick={handleSubcategory}>Mystery</li>
+                    <li onClick={handleSubcategory}>Science fiction</li>
+                    <li onClick={handleSubcategory}>Suspense/Thriller</li>
+                    <li onClick={handleSubcategory}>Western</li>
+                    <li onClick={handleSubcategory}>Mythology</li>
                 </ul>
             </div>
+            <div className="sub-category-nav">
+                <ul className="sub-cat">
+                    <li onClick={handleSubcategory}>Biography/Autobiography</li>
+                    <li onClick={handleSubcategory}>Essay</li>
+                    <li onClick={handleSubcategory}>Textbook</li>
+                    <li onClick={handleSubcategory}>Narrative nonfiction</li>
+                    <li onClick={handleSubcategory}>Reference book</li>
+                </ul>
+            </div>
+            <div className='book-card-container'>{bookResults}</div>
         </div>
     )
 }
