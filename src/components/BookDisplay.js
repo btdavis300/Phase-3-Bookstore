04513247c916125e0c3a05ce38640fb23bf60b79
Brennan Davis <brennantd@gmail.com>
2022-08-24 21:08:13 -0600

    slight changes to Book Display page css

--- src/components/BookDisplay.js
+++ src/components/BookDisplay.js
@@ -6,12 +6,12 @@ function BookDisplay({ book }) {
         <div>
             <div id="header-box">
                 <div id="book-header-container">
-                    <h1 id="book-header">{book.name}</h1>
+                    <h1 id="book-header"><em>{book.name}</em></h1>
                 </div>
             </div>
             <div id="full-display">
                 <div id="book-display-left-details">
-                    <img src={book.image} alt={book.name} width="300px" height="450px"></img>
+                    <img src={book.image} alt={book.name} width="280px" height="420px"></img>
                 </div>
                 <div id="book-display-right-details">
                     <div id="right-top-detail-display">
@@ -21,7 +21,7 @@ function BookDisplay({ book }) {
                                 <li><em>written by: {book.author}</em></li>
                                 <li>genre: {book.genre}</li>
                                 <li>year: {book.published_date}</li>
-                                <li><strong>${book.price}</strong></li>
+                                <li id="book-display-price"><strong>${book.price}</strong></li>
                             </ul>
                             <button id="add-to-cart">Add to Cart</button>
                         </section>
