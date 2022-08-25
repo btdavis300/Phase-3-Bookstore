a45eaeb6edec99225b38e44a8bdab16875d31da8
Brennan Davis <brennantd@gmail.com>
2022-08-25 14:26:41 -0600

    table click function

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -30,20 +30,21 @@ function Admin({ books }) {
             </div>
             <div id="table-container">
                 <table>
-                    <tr>
-                        <th></th>
-                        <th id="id-column">Book ID</th>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th id="genre-column">Genre</th>
-                        <th id="published-date-column">Published Date</th>
-                        <th id="price-column">Book Price</th>
-                        <th>Uploaded Date</th>
-                        <th>Updated At</th>
-                        <th>Image URL</th>
-                        <th>Book Description</th>
-                    </tr>
-                    {bookDatabaseTable}
+                    <tbody>
+                        <tr id="book-headers">
+                            <th id="id-column">Book ID</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th id="genre-column">Genre</th>
+                            <th id="published-date-column">Published Date</th>
+                            <th id="price-column">Book Price</th>
+                            <th>Uploaded Date</th>
+                            <th>Updated At</th>
+                            <th>Image URL</th>
+                            <th>Book Description</th>
+                        </tr>
+                        {bookDatabaseTable}
+                    </tbody>
                 </table>
             </div>
         </div>
